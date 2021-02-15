a2c8a53ecf8149a2c0b39cbf9cb94dc5d7930875
sumanthsumu1418 <sumanthsumu1418@users.noreply.github.com>
2021-02-15 05:58:14 +0530

    first commit

---
+++ logic.js
@@ -0,0 +1,26 @@
+var screen = document.getElementById('screen')
+buttons = document.querySelectorAll('button')
+var screenValue=''
+for(item of buttons){
+    item.addEventListener('click',(e)=>{
+        buttonText = e.target.innerText;
+        if(buttonText == 'x'){
+            buttonText = '*' 
+            screenValue  += buttonText; 
+            screen.value = screenValue;
+        }
+        else if (buttonText =='C') {
+            screenValue  += ''; 
+            screen.value = screenValue;
+        }
+        else if (buttonText =='=') {
+            screen.value = eval(screenValue);
+        }
+        else{
+            screenValue  += buttonText; 
+            screen.value = screenValue;
+        }
+        
+    }
+    )
+}
